29c829d61a378b7340f26bb2a9906a9f785b23b5
darina <darinakokoreva763@gmail.com>
2025-08-18 22:51:16 +0300

    HW Most Dangerous Target

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Model;
 using Model.Runtime.Projectiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Utilities;
+using Model.Runtime;
 
 namespace UnitBrains.Player
 {
@@ -12,6 +18,7 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
+
 
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
@@ -43,8 +50,32 @@ namespace UnitBrains.Player
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            Vector2Int currentPosition = base.GetNextStep();
+            List<Vector2Int> selectedTargets = SelectTargets();
+
+            if (selectedTargets.Count == 0)
+            {
+                return currentPosition;
+            }
+
+            Vector2Int target = selectedTargets[0];
+
+
+            if (Vector2Int.Distance(currentPosition, target) <= 1)// Если цель в зоне атаки не двигаемся
+            {
+                return currentPosition;
+            }
+
+            Vector2Int nextStep = currentPosition.CalcNextStepTowards(target);
+
+
+            return nextStep;
+
         }
+
+
+
+
 
         protected override List<Vector2Int> SelectTargets()
         {
@@ -53,27 +84,51 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             ///
 
-            List<Vector2Int> result = GetReachableTargets();
-            var minDistance = float.MaxValue;
-            Vector2Int closeTarget = default;
-            foreach (Vector2Int target in result)
-            {
-                float distance = DistanceToOwnBase(target);
-                if (distance<minDistance)
-                {
-                    minDistance = distance;
-                    closeTarget = target;
+            IEnumerable<Vector2Int> allTargets = GetAllTargets();
+            List<Vector2Int> result = GetReachableTargets().ToList();
+            List<Vector2Int> unreachableTargets = new List<Vector2Int>(); // Цели вне зоны досягаемости
 
+            
+            if (allTargets.Any())// Если есть цели
+            {
+                float minDistance = float.MaxValue;// Находим ближайшую цель 
+                Vector2Int closestTarget = default;
+
+                foreach (Vector2Int target in allTargets)
+                {
+                    float distance = DistanceToOwnBase(target);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestTarget = target;
+                    }
+                }
+
+                if (result.Contains(closestTarget))// Проверяем, достижима ли цель
+                {
+
+                    result.Clear();// Если достижима - оставляем только её
+                    result.Add(closestTarget);
+                }
+                else
+                {
+
+                    unreachableTargets.Add(closestTarget);
+                    result.Clear(); // Очищаем достижимые цели
                 }
             }
-            if (closeTarget != default) {
-                result.Clear();
-                result.Add(closeTarget);
+
+            else // Если целей нет - добавляем базу противника как следующую цель
+            {
+                int enemyPlayerId = IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId;
+                Vector2Int enemyBase = runtimeModel.RoMap.Bases[enemyPlayerId];
+                result.Add(enemyBase);
             }
-            
+
+
             return result;
 
-            
+
             ///////////////////////////////////////
         }
 
